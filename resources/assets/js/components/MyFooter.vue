6e518e66b2d0c96fd69a1a80a840dae1d12eb604
Jari Rengeling <jarirengeling@outlook.com>
2018-04-23 11:33:47 +0200

    Full project

---
+++ resources/assets/js/components/MyFooter.vue
@@ -0,0 +1,12 @@
+<template>
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Vuejs Phonebook App</strong> by <a href="https://jgthms.com">Jari Rengeling</a>. Access the source code
+        <a href="http://opensource.org/licenses/mit-license.php">Github.com/JariRengeling</a>.
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
